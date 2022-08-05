4e52ab6ae1b34d5bc64dde21c32e1a3de2c5f782
Rebecca Basquez <beckycb78@gmail.com>
2022-08-04 21:06:27 -0500

    Express Session Part 1

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/nucampsite';
@@ -32,10 +34,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
 function auth(req, res, next) {
-    if (!req.signedCookies.user) {
+    console.log(req.session);
+
+    if (!req.session.user) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
             const err = new Error('You are not authenticated!');
@@ -43,12 +54,12 @@ function auth(req, res, next) {
             err.status = 401;
             return next(err);
         }
-  
+
         const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
         const user = auth[0];
         const pass = auth[1];
         if (user === 'admin' && pass === 'password') {
-            res.cookie('user', 'admin', {signed: true});
+            req.session.user = 'admin';
             return next(); // authorized
         } else {
             const err = new Error('You are not authenticated!');
@@ -57,7 +68,7 @@ function auth(req, res, next) {
             return next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
             return next();
         } else {
             const err = new Error('You are not authenticated!');
